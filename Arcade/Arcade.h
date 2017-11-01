d8bcaa81b0264fdd2f9697f83eb06763d8f1964f
Aaron Wright <aaron.c.wright@icloud.com>
2017-11-01 05:30:28 -0500

    Initial Commit

---
+++ Arcade/Arcade.h
@@ -0,0 +1,19 @@
+//
+//  Arcade.h
+//  Arcade
+//
+//  Created by A.C. Wright Design on 11/1/17.
+//  Copyright © 2017 A.C. Wright Design. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Arcade.
+FOUNDATION_EXPORT double ArcadeVersionNumber;
+
+//! Project version string for Arcade.
+FOUNDATION_EXPORT const unsigned char ArcadeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Arcade/PublicHeader.h>
+
+
