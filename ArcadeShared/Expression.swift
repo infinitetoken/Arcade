0984cadfce6f89dac29ab600b2f520b829b3dd2f
Aaron Wright <aaron.c.wright@icloud.com>
2017-11-28 15:37:17 -0600

    Add contains to Comparison

--- ArcadeShared/Expression.swift
+++ ArcadeShared/Expression.swift
@@ -14,7 +14,8 @@ public typealias Constant = CustomStringConvertible
 public enum Expression {
     case equal(KeyPath, Constant)
     case notEqual(KeyPath, Constant)
-    case comparison(KeyPath, Comparison, Constant)
+    case contains(KeyPath, Constant)
+    case comparison(KeyPath, Comparison, Constant, NSComparisonPredicate.Options)
     case isNil(KeyPath)
     case isNotNil(KeyPath)
 }
@@ -24,12 +25,13 @@ extension Expression {
         switch self {
             case let .equal(keyPath, constant): return NSPredicate(format: "%K = %@", keyPath, constant.description)
             case let .notEqual(keyPath, constant): return NSPredicate(format: "%K != %@", keyPath, constant.description)
-            case let .comparison(keyPath, comparison, constant):
+            case let .contains(keypath, constant): return NSPredicate(format: "%K contains %@", keypath, constant.description)
+            case let .comparison(keyPath, comparison, constant, options):
                 let leftExpression = NSExpression(forKeyPath: keyPath)
                 let rightExpression = NSExpression(forConstantValue: constant)
                 let modifier = NSComparisonPredicate.Modifier.direct
                 let type = comparison.type()
-                return NSComparisonPredicate(leftExpression: leftExpression, rightExpression: rightExpression, modifier: modifier, type: type, options: [])
+                return NSComparisonPredicate(leftExpression: leftExpression, rightExpression: rightExpression, modifier: modifier, type: type, options: options)
             case let .isNil(keyPath): return NSPredicate(format: "%K = nil", keyPath)
             case let .isNotNil(keyPath): return NSPredicate(format: "%K != nil", keyPath)
         }
@@ -41,7 +43,8 @@ extension Expression: CustomStringConvertible {
         switch self {
             case let .equal(keyPath, constant): return "\(keyPath) \(Comparison.equalTo) \(constant)"
             case let .notEqual(keyPath, constant): return "\(keyPath) \(Comparison.notEqualTo) \(constant)"
-            case let .comparison(keyPath, comparison, constant): return "\(keyPath) \(comparison) \(constant)"
+            case let .contains(keyPath, constant): return "\(keyPath) \(Comparison.contains) \(constant)"
+        case let .comparison(keyPath, comparison, constant, options): return options.rawValue == 0 ? "\(keyPath) \(comparison) \(constant)" : "\(keyPath) \(comparison)[\(options)] \(constant)"
             case let .isNil(keyPath): return "\(keyPath) \(Comparison.equalTo) nil"
             case let .isNotNil(keyPath): return "\(keyPath) \(Comparison.notEqualTo) nil"
         }
