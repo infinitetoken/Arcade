d2702e2ca51df2cdbc23afff999ea4c7842eff71
pfoster <paulfostergameroom@gmail.com>
2017-12-17 18:51:01 -0600

    bug fix

--- ArcadeShared/InMemoryAdapter.swift
+++ ArcadeShared/InMemoryAdapter.swift
@@ -205,3 +205,45 @@ extension InMemoryAdapter: Adapter {
     
 }
 
+
+struct ActionStack {
+    
+    enum Operation {
+        case insert
+        case update
+        case delete
+    }
+    
+    private var storables: [[Storable]] = []
+    private var operations: [Operation] = []
+    private var tables: [Table] = []
+    
+    mutating func push(storables: [Storable], operation: Operation, table: Table) {
+        self.storables.append(storables)
+        self.operations.append(operation)
+        self.tables.append(table)
+    }
+    
+    mutating func pop<I: Storable>() -> (storables: [I], operation: Operation) {
+        guard let storables = self.storables.popLast() as? [I],
+            let operation = self.operations.popLast()
+            else { fatalError() }
+        return (storables, operation)
+    }
+    
+    mutating func popOperation() -> Operation? { return self.operations.popLast() }
+    mutating func popStorables() -> [Storable] {
+        guard let storables = self.storables.last else { fatalError() }
+        self.storables.removeLast()
+        return storables
+    }
+    mutating func popTable() -> Table? { return self.tables.popLast() }
+    
+    func peek() -> (storables: [Storable], operation: Operation) {
+        guard let storables = self.storables.last,
+            let operation = self.operations.last
+            else { fatalError() }
+        return (storables, operation)
+    }
+}
+
